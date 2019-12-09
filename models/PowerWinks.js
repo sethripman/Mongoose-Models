8868346305cb5d6fe9ea7282d596ee51cb0e4b31
sethripman <sethripman@gmail.com>
2019-12-09 15:33:15 -0800

    copying first model as template

---
+++ models/PowerWinks.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  tignometerRating: {
+    type: String,
+    required: true
+  },
+  coporability: {
+    type: String,
+    required: true
+  },
+  slugsPresent: {
+    type: Boolean,
+    required: true
+  },
+  slugFlavor: {
+    type: String,
+    required: true
+  },
+  isSkurb: {
+    type: Boolean,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Bafmodad', schema);
